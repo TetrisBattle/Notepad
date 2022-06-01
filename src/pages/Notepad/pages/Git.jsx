6c6e31cb33f76cc1e13cc2076b2bb61539b2fb6b
Thien <thien5596@gmail.com>
2022-06-01 22:30:52 +0300

    Discard Material-UI Accordion and create my own version

--- src/pages/Notepad/pages/Git.jsx
+++ src/pages/Notepad/pages/Git.jsx
@@ -1,60 +1,22 @@
 import { useState } from 'react'
-import {
-	Accordion,
-	AccordionSummary,
-	AccordionDetails
-} from '@mui/material'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-
 import Navbar from 'components/Navbar'
 import { git } from '../data/git.json'
+import ExpandBars from 'components/ExpandBar'
+
+const pages = [
+	'Init',
+	'Remote',
+	'Branch',
+	'Commit',
+	'Changes',
+	'Stash',
+	'Reset',
+	'Info'
+]
 
 export default function Git() {
-	const pages = [
-		'Init',
-		'Remote',
-		'Branch',
-		'Commit',
-		'Changes',
-		'Stash',
-		'Reset',
-		'Info'
-	]
-
 	const [selectedPage, setSelectedPage] = useState(pages[0])
-	const [expanded, setExpanded] = useState(false);
-
-	const getCommands = () => {
-		const getCommands = (commands) => (
-			commands?.map(({ command, description, details }) => (
-					<Accordion
-						key={command}
-						expanded={expanded === details}
-						onChange={() => {
-							if (details) setExpanded(expanded === details ? false : details)
-						}}
-						disableGutters
-					>
-						<AccordionSummary
-							expandIcon={<ExpandMoreIcon />}
-							sx={{
-								'svg': { visibility: details ? 'visible': 'hidden' },
-								':hover': { cursor: details ? 'pointer': 'default !important' }
-							}}
-						>
-							<p>{command}</p>
-							<p>{description}</p>
-						</AccordionSummary>
-						<AccordionDetails>
-							<p>{details}</p>
-						</AccordionDetails>
-					</Accordion>
-				)
-			)
-		)
-
-		return getCommands(git[selectedPage.toLowerCase()])
-	}
+	const currentPageCommands = git[selectedPage.toLowerCase()]
 
 	return (
 		<div className="git">
@@ -65,7 +27,9 @@ export default function Git() {
 			/>
 			<main>
 				<h1>Git commands</h1>
-				<section>{getCommands()}</section>
+				<section>
+					<ExpandBars commands={currentPageCommands} />
+				</section>
 			</main>
 		</div>
 	)
--- src/styles/MuiTheme.ts
+++ src/styles/MuiTheme.ts
@@ -4,16 +4,16 @@ const theme = createTheme({
 	palette: {
 		mode: 'dark',
 	},
-	components: {
-		MuiAccordionDetails: {
-			styleOverrides: {
-				root: {
-					backgroundColor: '#303030',
-					padding: '.75rem 1rem'
-				}
-			},
-		},
-	},
+	// components: {
+	// 	MuiAccordionDetails: {
+	// 		styleOverrides: {
+	// 			root: {
+	// 				backgroundColor: '#303030',
+	// 				padding: '.75rem 1rem',
+	// 			}
+	// 		},
+	// 	},
+	// },
 })
 
 export default theme
